17aeecd2fa905a416b6407bbe57e94309de94bfd
Xavier Xaimo <xmaimo@gmail.com>
2018-09-18 12:43:33 +0200

    Posts route updated

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route   GET api/posts/
 // @desc    Tests post route
 // @access  Public
-router.get('/', (req, res) => res.send('Hello Posts'));
+router.get('/', (req, res) => res.send('Hello Posts!!!!'));
 
 module.exports = router;
