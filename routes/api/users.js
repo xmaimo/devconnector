4cd52fffd95923cb30e70e622a090e3723f6a3dd
Xavier Xaimo <xmaimo@gmail.com>
2018-09-20 16:34:38 +0200

    Added passport-jwt Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,84 @@
+const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
+
+const keys = require('../../config/keys');
+const User = require('../models/User');
 
 // @route   GET api/users/
 // @desc    Tests users route
 // @access  Public
 router.get('/', (req, res) => res.json({ msg: 'Hello Router' }));
+
+/**
+ * @route   POST api/users/register
+ * @desc    Register user
+ * @access  Public
+ */
+router.post('/register', async (req, res) => {
+  let newUser = await User.findOne({ email: req.body.email });
+  if (newUser) return res.status(400).json({ email: 'User already registerd' });
+
+  const avatar = gravatar.url(req.body.email, { s: '200', r: 'pg', d: 'mm' });
+
+  newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    avatar
+  });
+
+  // password encryption
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(newUser.password, salt);
+  newUser.password = hash;
+
+  await newUser.save();
+
+  res.json(newUser);
+});
+
+// @route   POST api/users/login
+// @desc    Login user / Returning JWT
+// @access  Public
+router.post('/login', async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Check for User
+  let user = await User.findOne({ email });
+  if (!user) return res.status(404).json({ email: 'User not found' });
+
+  // Check for password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (isMatch) {
+    // sign token
+    const token = jwt.sign(
+      { id: user._id, name: user.name, avatar: user.avatar }, // payload
+      keys.secretKey,
+      { expiresIn: 7200 } // expires in seconds
+    );
+
+    res.json({ success: true, token: 'Bearer ' + token });
+  } else {
+    return res.status(400).json({ password: 'Password Incorrect' });
+  }
+
+  /**
+   * @route   GET api/users/current
+   * @desc    Return current user
+   * @access  Private
+   */
+  router.get(
+    '/current',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      res.json(req.user);
+    }
+  );
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profiles');
 const posts = require('./routes/api/posts');
@@ -23,9 +23,13 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Routes
-app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
